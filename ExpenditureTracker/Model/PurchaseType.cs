14ee61c8157a9ad087adbe7c899a98553a0871cf
gregkorte <gryphionsroost@yahoo.com>
2015-02-12 13:44:43 -0600

    Initial setup classes and context.

---
+++ ExpenditureTracker/Model/PurchaseType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenditureTracker.Model
+{
+    class PurchaseType
+    {
+        public int ReceiptId { get; set; }
+        public int PurchaseTypeId { get; set; }
+        public string PurchaseType { get; set; }
+
+        public PurchaseType(int PurchaseTypeId, string PurchaseType)
+        {
+            this.PurchaseTypeId = PurchaseTypeId;
+            this.PurchaseType = PurchaseType;
+        }
+    }
+}
---
+++ ExpenditureTracker/Model/Receipt.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenditureTracker.Model
+{
+    public class Receipt
+    {
+        public int ReceiptId { get; set; }
+        public string Date { get; set; }
+        public double SalesTax { get; set; }
+        public double SalesTotal { get; set; }
+
+        public Receipt(string Date, double SalesTax, double TotalSales)
+        {
+            this.Date = Date;
+            this.SalesTax = SalesTax;
+            this.SalesTotal = SalesTotal;
+        }
+    }
+}
